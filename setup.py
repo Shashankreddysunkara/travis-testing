85efd7bb3d6625e9d0e5ba9a8f1528f0b89ea293
Neil <neil@dip.sun.ac.za>
2016-02-08 12:40:20 +0200

    Minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,39 @@
+import sys
+
+from setuptools import setup
+
+
+if sys.platform.startswith("darwin"):
+    # Do something different for mac
+    pass
+
+
+setup(
+    name="travis_testing",
+    version="0.0.1",
+    url='http://github.com/CTPUG/pygame_cffi',
+    license='MIT',
+    description="A package for testing travis configurations.",
+    long_description=open('README.rst', 'r').read(),
+    packages=['thing'],
+    include_package_data=True,
+    scripts=[
+    ],
+    setup_requires=[
+        'cffi>=1.0.3',
+    ],
+    cffi_modules=cffi_modules,
+    install_requires=[
+        'cffi>=1.0.3',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
